24b4ef570e719f31e71f2d4f7ecad61e774a6e7f
veljko <veljko@precip.umd.edu>
2024-05-20 12:52:39 -0400

    clean __init.py__ to allow for regular CPU-based processing

--- noaagprof/__init__.py
+++ noaagprof/__init__.py
@@ -14,8 +14,69 @@ from gprof_nn import sensors
 import numpy as np
 import torch
 import xarray as xr
+import h5py 
 
-import h5py # to read in GCOM-W1 AMSR2 level1b file. Used by read_noaa_gcom_l1b_file (veljko)
+def read_noaa_gcom_l1b_file(file_path):
+# A function to read in and resample GCOMW1-AMSR2 TBs
+
+  # read in the data
+  with h5py.File(file_path, 'r') as file:
+
+    # Read only the necessary vars
+    dataset_vars=                             \
+    ["Brightness Temperature (10.7GHz,V)"   , \
+     "Brightness Temperature (10.7GHz,H)"   , \
+     "Brightness Temperature (18.7GHz,V)"   , \
+     "Brightness Temperature (18.7GHz,H)"   , \
+     "Brightness Temperature (23.8GHz,V)"   , \
+     "Brightness Temperature (23.8GHz,H)"   , \
+     "Brightness Temperature (36.5GHz,V)"   , \
+     "Brightness Temperature (36.5GHz,H)"   , \
+     "Brightness Temperature (89.0GHz-A,V)" , \
+     "Brightness Temperature (89.0GHz-A,H)" , \
+     "Latitude of Observation Point for 89A", \
+     "Longitude of Observation Point for 89A"]
+
+    in_count=0 # channel counter, for indexing purposes
+
+    # loop over variables to read from l1b and store into arrays
+    for var in dataset_vars:
+      if var in file:
+        data_raw = file[var]
+      else:
+        print("Dataset", var, "not found in the file.")
+
+      # Check if the dataset has a scale factor attribute
+      if 'SCALE FACTOR' in data_raw.attrs:
+        scale_factor = data_raw.attrs['SCALE FACTOR']
+
+        # Apply the scale factor to obtain the original data
+        data = data_raw * scale_factor
+
+      # declare output array. Use the 1st channel to determine the size (i.e., # of scans)
+      if var == dataset_vars[0]:
+        tbs_out  = np.empty((data.shape[0],486,10),dtype=float)
+        lon      = np.empty((data.shape[0],486)   ,dtype=float)
+        lat      = np.empty((data.shape[0],486)   ,dtype=float)
+
+      # for all low freq. channels (0-7) replicate pixels along the scanline to match the high freq. sampling
+      if in_count <= 7:
+        data = np.repeat(data, 2, axis=1)
+
+      # Store data into the output arrays
+      if in_count <= 9:
+        tbs_out [:,:,in_count]=np.array(data)
+
+      if in_count == 10:
+        lat[:,:]=np.array(data)
+
+      if in_count == 11:
+        lon[:,:]=np.array(data)
+
+      # increase channel count
+      in_count=in_count+1
+
+  return tbs_out, lat, lon
 
 
 class InputLoader:
@@ -43,7 +104,8 @@ class InputLoader:
 
         # If input is a folder, find all .HDF5 files.
         if isinstance(inputs, Path) and inputs.is_dir():
-            self.files = sorted(list(inputs.glob("**/*.HDF5")))
+           #self.files = sorted(list(inputs.glob("**/*.HDF5")))
+            self.files = sorted(list(inputs.glob("**/*.h5")))
         else:
             # If input is a list assume its list of input files.
             if isinstance(inputs, list):
@@ -78,73 +140,7 @@ class InputLoader:
             to determine the output filename and to include ancillary data in the retrieval results.
         """
 
-        # Veljko: a function to read in and resample GCOMW1-AMSR2 TBs
-        def read_noaa_gcom_l1b_file(file_path):
-        
-          # read in the data
-          with h5py.File(file_path, 'r') as file:
-        
-            # Read specific dataset
-            dataset_name=                            \
-            ["Brightness Temperature (10.7GHz,V)"  , \
-             "Brightness Temperature (10.7GHz,H)"  , \
-             "Brightness Temperature (18.7GHz,V)"  , \
-             "Brightness Temperature (18.7GHz,H)"  , \
-             "Brightness Temperature (23.8GHz,V)"  , \
-             "Brightness Temperature (23.8GHz,H)"  , \
-             "Brightness Temperature (36.5GHz,V)"  , \
-             "Brightness Temperature (36.5GHz,H)"  , \
-             "Brightness Temperature (89.0GHz-A,V)", \
-             "Brightness Temperature (89.0GHz-A,H)", \
-             "Latitude of Observation Point for 89A", \
-             "Longitude of Observation Point for 89A"]
-        
-            ch_count=0 # channel counter, for indexing purposes
-        
-            # loop over variables to read from l1b and store into array
-            for tb in dataset_name:
-              if tb in file:
-                data_raw = file[tb]
-              else:
-                print("Dataset", tb, "not found in the file.")
-        
-              # Check if the dataset has a scale factor attribute
-              if 'SCALE FACTOR' in data_raw.attrs:
-                scale_factor = data_raw.attrs['SCALE FACTOR']
-        
-                # Apply the scale factor to obtain the original data
-                data = data_raw * scale_factor
-        
-              #else:
-                #print("Dataset", tb, "does not have a scale factor.")
-        
-              # declare output array. Use the 1st channel to determine the size (i.e., # of scans)
-              if tb == dataset_name[0]:
-                data_out = np.empty((data.shape[0],486,len(dataset_name)),dtype=float)
-        
-              # for all low freq. channels (0-7) replicate pixels along the scanline to match the high freq. sampling
-              if ch_count <= 7:
-                data = np.repeat(data, 2, axis=1)
-        
-              # Store data into the output array
-              data_out[:,:,ch_count]=np.array(data)
-        
-              # increase channel count
-              ch_count=ch_count+1
-        
-          tbs_out=data_out[:,:,0:10]
-          lon    =data_out[:,:,-1]
-          lat    =data_out[:,:,-2]
-
-          return tbs_out, lat, lon
-
-
         input_file = Path(input_file)
-     #  l1c_file = L1CFile(input_file)                     # veljko
-     #  l1c_file = L1CFile('../data_gcom/1C.GCOMW1.AMSR2.XCAL2016-V.20240414-S060351-E074243.063343.V07A.HDF5') # veljko
-     #  sensor = l1c_file.sensor                    
-     #  input_data = run_preprocessor(input_file, sensor)  # veljko
-        input_data = None                                  # veljko
         filename = input_file.name
 
         # Tbs and angles must be expanded to the 15 GPROF channels.
@@ -164,9 +160,9 @@ class InputLoader:
         #      9 |  89        |  "H"
         #
 
-        tbs, lat, lon = read_noaa_gcom_l1b_file(input_file)      # veljko
-        tbs[tbs < 0] = np.nan
-        tbs_full = np.nan * np.zeros((tbs.shape[:2] +(15,)), dtype=np.float32)
+        tbs, lat, lon = read_noaa_gcom_l1b_file(input_file)     
+        tbs[tbs < 0]  = np.nan
+        tbs_full      = np.nan * np.zeros((tbs.shape[:2] +(15,)), dtype=np.float32)
         tbs_full[..., :10] = tbs  # populate only the first 10 channels (AMSR2)
 
         # Order of dimensions should be [batch, channels, scans, pixel].
@@ -176,8 +172,6 @@ class InputLoader:
         return {
             "brightness_temperatures": torch.tensor(tbs_full)
         }, filename, lat, lon
-       # }, filename, input_data
-
 
     def __len__(self):
         """
@@ -199,6 +193,8 @@ class InputLoader:
             lat, lon 
            # preprocessor_data: xr.Dataset
     ) -> xr.Dataset:
+
+
         """
         This function is called after inference has been performed on all input data from a given file.
         The purpose of this function is to define the output name and customize the format and content of
@@ -252,7 +248,7 @@ class InputLoader:
             .replace("1C", "2A")
             .replace("HDF5", "nc")
             .replace("h5", "nc")
-            .replace("L1B", "noaagprof_AMSR2_PRECIP_v4_v0")
+            .replace("GAASP-L1B", "noaagprof_AMSR2_PRECIP_v4r0")
         )
 
         # Return results as xr.Dataset and filename to use to save data.
